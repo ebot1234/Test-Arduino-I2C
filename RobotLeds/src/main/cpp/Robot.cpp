b2ad690cdef29ad3b6350c8316d3e82c10d72511
ebot1234 <ebot14rocks@gmail.com>
2019-02-21 12:20:09 -0500

    added arduino data to store returned data from the arduino

--- RobotLeds/src/main/cpp/Robot.cpp
+++ RobotLeds/src/main/cpp/Robot.cpp
@@ -14,7 +14,8 @@ using namespace std;
 Robot::Robot() {
   
 }
-uint8_t leds;
+uint8_t leds[1];
+uint8_t arduino_data[1];
 
 frc::I2C arduino = {frc::I2C::Port::kOnboard, 2};
 frc::Joystick stick0{0};
@@ -34,24 +35,24 @@ void setLedsBeforeMatch(){
   if(frc::DriverStation::IsFMSAttached() == true || frc::DriverStation::IsDSAttached() == true){
     if(frc::DriverStation::Alliance::kRed){
       leds[0] = 1;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       std::cout << "Robot is connected and is red alliance\n" << endl;
     }
     else{
       leds[0] = 9;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       std::cout << "Robot is connected and no alliance is selected\n";
     }
   }
   if(frc::DriverStation::IsFMSAttached() == true ||frc::DriverStation::IsDSAttached() == true){
     if(frc::DriverStation::Alliance::kBlue){
       leds[0] = 2;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       std::cout << "Robot is connected and is blue alliance\n" << endl;
     }
     else{
       leds[0] = 9;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       std::cout << "Robot is connected and no alliance is selected\n";
     }
   }
@@ -61,15 +62,15 @@ void setLedsDuringMatch(int mode){
   switch(mode){
     case 1:
       leds[0] = 1;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       break;
     case 2:
       leds[0] = 2;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       break;
     case 3:
       leds[0] = 3;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       break;
     case 4:
       leds[0] = 4;
@@ -77,23 +78,23 @@ void setLedsDuringMatch(int mode){
       break;
     case 5:
       leds[0] = 5;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       break;
     case 6:
       leds[0] = 6;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       break;
     case 7:
       leds[0] = 7;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       break;
     case 8:
       leds[0] = 8;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       break;
     case 9:
       leds[0] = 9;
-      arduino.Transaction(leds, 1, NULL, NULL);
+      arduino.Transaction(leds, 1, arduino_data, 1);
       break;
   }
 }
