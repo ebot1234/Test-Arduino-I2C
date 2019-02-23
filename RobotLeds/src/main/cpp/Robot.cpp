3051cf6508b85fd1f4a5fe3e585441b94a44bcd8
ebot1234 <ebot14rocks@gmail.com>
2019-02-23 18:07:31 -0500

    did something

--- RobotLeds/src/main/cpp/Robot.cpp
+++ RobotLeds/src/main/cpp/Robot.cpp
@@ -16,7 +16,13 @@ using namespace std;
 Robot::Robot() {
   
 }
+<<<<<<< HEAD
 frc::I2C I2Channel = {frc::I2C::Port::kOnboard, 2};
+=======
+uint8_t leds;
+
+frc::I2C arduino = {frc::I2C::Port::kOnboard, 2};
+>>>>>>> parent of b2ad690... added arduino data to store returned data from the arduino
 frc::Joystick stick0{0};
 frc::DigitalInput limBot(0);
 
@@ -25,6 +31,7 @@ void Robot::RobotInit() {
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 }
+<<<<<<< HEAD
 //This function is for sending data through I2C
 void I2CWrite(uint8_t data){
   I2Channel.Write(8, data);
@@ -59,6 +66,75 @@ void setLeds(int ledMode){
 	case 7:
     I2CWrite(121);
     break; // wipe to amber
+=======
+
+void setLedsBeforeMatch(){
+//Sets the leds if FMS or DS is present and to the alliance color when robot is ready
+  if(frc::DriverStation::IsFMSAttached() == true || frc::DriverStation::IsDSAttached() == true){
+    if(frc::DriverStation::Alliance::kRed){
+      leds[0] = 1;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      std::cout << "Robot is connected and is red alliance\n" << endl;
+    }
+    else{
+      leds[0] = 9;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      std::cout << "Robot is connected and no alliance is selected\n";
+    }
+  }
+  if(frc::DriverStation::IsFMSAttached() == true ||frc::DriverStation::IsDSAttached() == true){
+    if(frc::DriverStation::Alliance::kBlue){
+      leds[0] = 2;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      std::cout << "Robot is connected and is blue alliance\n" << endl;
+    }
+    else{
+      leds[0] = 9;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      std::cout << "Robot is connected and no alliance is selected\n";
+    }
+  }
+}
+
+void setLedsDuringMatch(int mode){
+  switch(mode){
+    case 1:
+      leds[0] = 1;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      break;
+    case 2:
+      leds[0] = 2;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      break;
+    case 3:
+      leds[0] = 3;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      break;
+    case 4:
+      leds[0] = 4;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      break;
+    case 5:
+      leds[0] = 5;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      break;
+    case 6:
+      leds[0] = 6;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      break;
+    case 7:
+      leds[0] = 7;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      break;
+    case 8:
+      leds[0] = 8;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      break;
+    case 9:
+      leds[0] = 9;
+      arduino.Transaction(leds, 1, NULL, NULL);
+      break;
+>>>>>>> parent of b2ad690... added arduino data to store returned data from the arduino
   }
 }
 
